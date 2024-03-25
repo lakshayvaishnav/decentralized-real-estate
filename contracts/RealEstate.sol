5ea5771cc83f3a4978cc6794cb0ef990b8658701
thelakshayvaishnav <thelakshayvaishnav@gmail.com>
2024-03-26 00:05:50 +0530

    completed project

--- contracts/RealEstate.sol
+++ contracts/RealEstate.sol
@@ -1,27 +1,27 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import '@openzeppelin/contracts/utils/Counters.sol';
+import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+import '@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol';
 
 contract RealEstate is ERC721URIStorage {
-    using Counters for Counters.Counter;
-    Counters.Counter private _tokenIDs;
+  using Counters for Counters.Counter;
+  Counters.Counter private _tokenIds;
 
-    constructor() ERC721("RealEstate", "Real") {}
+  constructor() ERC721('Real Estate', 'REAL') {}
 
-    function mint(string memory tokenURI) public returns (uint256) {
-        _tokenIDs.increment();
+  function mint(string memory tokenURI) public returns (uint256) {
+    _tokenIds.increment();
 
-        uint256 newItemId = _tokenIDs.current();
-        _mint(msg.sender, newItemId);
-        _setTokenURI(newItemId, tokenURI);
+    uint256 newItemId = _tokenIds.current();
+    _mint(msg.sender, newItemId);
+    _setTokenURI(newItemId, tokenURI);
 
-        return newItemId;
-    }
+    return newItemId;
+  }
 
-    function totalSupply() public view returns (uint256) {
-        return _tokenIDs.current();
-    }
+  function totalSupply() external view returns (uint256) {
+    return _tokenIds.current();
+  }
 }
--- src/App.js
+++ src/App.js
@@ -7,52 +7,109 @@ import Search from './components/Search'
 import Home from './components/Home'
 
 // ABIs
-import RealEstate from './abis/RealEstate.json'
-import Escrow from './abis/Escrow.json'
+import RealEstateAbi from './abis/RealEstate.json'
+import EscrowAbi from './abis/Escrow.json'
 
 // Config
 import config from './config.json'
 
 function App() {
-  const [account, setAccount] = useState('')
+  const [provider, setProvider] = useState(null)
+  const [escrow, setEscrow] = useState(null)
 
-  const loadBloackchainData = async () => {
+  const [account, setAccount] = useState(null)
+
+  const [homes, setHomes] = useState([])
+  const [home, setHome] = useState({})
+  const [toggle, setToggle] = useState(false)
+
+  const loadBlockchainData = async () => {
     const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const signer = provider.getSigner()
+    setProvider(provider)
+
+    const realEstate = new ethers.Contract(
+      '0x5FbDB2315678afecb367f032d93F642f64180aa3',
+      RealEstateAbi,
+      signer,
+    )
+    // const supply = realEstate.totalSupply()
+    const name = await realEstate.name()
+    console.log(name)
+    // const totalSupply = await realEstate.totalSupply()
+    // const homes = []
+
+    // for (var i = 1; i <= totalSupply; i++) {
+    //   const uri = await realEstate.tokenURI(i)
+    //   const response = await fetch(uri)
+    //   const metadata = await response.json()
+    //   homes.push(metadata)
+    // }
+
+    // setHomes(homes)
+
+    const escrow = new ethers.Contract(
+      '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512',
+      EscrowAbi,
+      provider,
+    )
+    setEscrow(escrow)
 
     window.ethereum.on('accountsChanged', async () => {
-      const accounts = window.ethereum.request({ method: 'requestAccounts' })
+      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
       const account = ethers.utils.getAddress(accounts[0])
       setAccount(account)
     })
   }
+
   useEffect(() => {
-    loadBloackchainData()
+    loadBlockchainData()
   }, [])
+
+  const togglePop = (home) => {
+    setHome(home)
+    toggle ? setToggle(false) : setToggle(true)
+  }
 
   return (
     <div>
       <Navigation account={account} setAccount={setAccount} />
       <Search />
+
       <div className='cards__section'>
-        <h3>Homes For You !</h3>
+        <h3>Homes For You</h3>
+
         <hr />
 
         <div className='cards'>
-          <div className='card'>
-            <div className='card__image'>
-              <img alt='house' />
+          {homes.map((home, index) => (
+            <div className='card' key={index} onClick={() => togglePop(home)}>
+              <div className='card__image'>
+                <img src={home.image} alt='Home' />
+              </div>
+              <div className='card__info'>
+                <h4>{home.attributes[0].value} ETH</h4>
+                <p>
+                  <strong>{home.attributes[2].value}</strong> bds |
+                  <strong>{home.attributes[3].value}</strong> ba |
+                  <strong>{home.attributes[4].value}</strong> sqft
+                </p>
+                <p>{home.address}</p>
+              </div>
             </div>
-            <div className='card__info'>
-              <h4>ETH</h4>
-              <p>
-                <strong></strong>bds|
-                <strong></strong> ba |<strong></strong> sqft
-              </p>
-              <p>home address</p>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
+
+      {toggle && (
+        <Home
+          home={home}
+          provider={provider}
+          account={account}
+          escrow={escrow}
+          togglePop={togglePop}
+        />
+      )}
     </div>
   )
 }
